34748486592dc1434779e60adfedb0ce20f4c3a9
Baidou2Star <SunJJ99@outlook.com>
2026-01-03 11:14:15 +0800

    first commit

---
+++ myChatQT/main.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+#include <QFile>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//加载qss
+    QFile qss(":/style/stylesheet.qss");
+
+    if( qss.open(QFile::ReadOnly))
+    {
+        qDebug("open success");
+        QString style = QLatin1String(qss.readAll());
+        a.setStyleSheet(style);
+        qss.close();
+    }else{
+        qDebug("Open failed");
+    }
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "myChatQT_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
