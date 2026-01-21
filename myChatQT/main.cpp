4a15afa65b6842d0cfe38a133f45b843db691fc5
Baidou <1424089348@qq.com>
2026-01-21 20:43:12 +0800

    remove ignored files from git tracking

--- myChatQT/main.cpp
+++ myChatQT/main.cpp
@@ -1,13 +1,12 @@
 #include "mainwindow.h"
-
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 #include <QFile>
+#include "global.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//加载qss
+
     QFile qss(":/style/stylesheet.qss");
 
     if( qss.open(QFile::ReadOnly))
@@ -17,18 +16,22 @@ int main(int argc, char *argv[])
         a.setStyleSheet(style);
         qss.close();
     }else{
-        qDebug("Open failed");
-    }
+         qDebug("Open failed");
+     }
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "myChatQT_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
+
+    // 获取当前应用程序的路径
+    QString app_path = QCoreApplication::applicationDirPath();
+    // 拼接文件名
+    QString fileName = "config.ini";
+    QString config_path = QDir::toNativeSeparators(app_path +
+                             QDir::separator() + fileName);
+
+    QSettings settings(config_path, QSettings::IniFormat);
+    QString gate_host = settings.value("GateServer/host").toString();
+    QString gate_port = settings.value("GateServer/port").toString();
+    gate_url_prefix = "http://"+gate_host+":"+gate_port;
+
     MainWindow w;
     w.show();
     return a.exec();
--- myChatQT/mainwindow.h
+++ myChatQT/mainwindow.h
@@ -2,36 +2,54 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include<logindialog.h>
-#include<registerdialog.h>
+#include "logindialog.h"
+#include "registerdialog.h"
+#include "resetdialog.h"
+#include "chatdialog.h"
 /******************************************************************************
  *
  * @file       mainwindow.h
- * @brief      主窗口
+ * @brief      主界面功能 Function
  *
- * @author     Baidou2Star
- * @date       2026/01/02
+ * @author     恋恋风辰
+ * @date       2024/02/27
  * @history
  *****************************************************************************/
-QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-QT_END_NAMESPACE
+
+enum UIStatus{
+    LOGIN_UI,
+    REGISTER_UI,
+    RESET_UI,
+    CHAT_UI
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
 public slots:
     void SlotSwitchReg();
+    void SlotSwitchLogin();
+    void SlotSwitchReset();
+    void SlotSwitchLogin2();
+    void SlotSwitchChat();
+    void SlotOffline();
+    void SlotExcepConOffline();
+
 private:
+    void offlineLogin();
     Ui::MainWindow *ui;
-    LoginDialog *_login_dlg;
-    RegisterDialog *_reg_dlg;
+    LoginDialog* _login_dlg;
+    RegisterDialog* _reg_dlg;
+    ResetDialog* _reset_dlg;
+    ChatDialog* _chat_dlg;
+    UIStatus _ui_status;
 };
+
 #endif // MAINWINDOW_H
